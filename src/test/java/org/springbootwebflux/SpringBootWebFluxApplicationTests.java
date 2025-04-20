d9e0f10e273215a117f412b8f65c3d6fcde3a7d1
leoromero <leonardo.vargas.ulsa@gmail.com>
2025-04-20 13:25:28 -0600

    Initial commit

---
+++ src/test/java/org/springbootwebflux/SpringBootWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springbootwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebFluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
